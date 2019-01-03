f4b344fddf796f8654df9c1cd118a85137286520
alexzhao201601 <alexzhao201601@gmail.com>
2019-01-03 19:13:01 +0800

    git auto commit

--- push.sh
+++ push.sh
@@ -1,1 +1,1 @@
-* */2 * * * cd /markliu/git-auto-commit git pull && /usr/bin/node add.js && git commit -a -m 'git auto commit' && git push origin master
+* */2 * * * cd /markliu/git-auto-commit git pull && /usr/bin/node add.js && git add * && git commit -a -m 'git auto commit' && git push origin master
