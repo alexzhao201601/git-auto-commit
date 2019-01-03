6a8907aba1ff63e22e30c6c5a1cad0ad6a0ee895
alexzhao201601 <46335501+alexzhao201601@users.noreply.github.com>
2019-01-03 22:42:37 +0800

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,1 +1,1 @@
-* */2 * * * cd /markliu/git-auto-commit && git fetch --all && git reset --hard origin/master && git pull && /usr/bin/node add.js && git add * && git commit -a -m 'git auto commit' && git push
+0 */2 * * * cd /markliu/git-auto-commit && git fetch --all && git reset --hard origin/master && git pull && /usr/bin/node add.js && git add * && git commit -a -m 'git auto commit' && git push
